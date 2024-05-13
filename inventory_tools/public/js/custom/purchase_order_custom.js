683d1f1075d64b280400d47ae08b5eccadf1a7a9
Tyler Matteson <tyler@agritheory.com>
2024-05-13 17:45:21 -0400

    wip: fix multi-company PO aggregation workflow frontend

--- inventory_tools/public/js/custom/purchase_order_custom.js
+++ inventory_tools/public/js/custom/purchase_order_custom.js
@@ -1,53 +1,145 @@
-// Copyright (c) 2023, AgriTheory and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Purchase Order', {
 	onload_post_render: frm => {
 		override_create_buttons(frm)
 	},
 	refresh: frm => {
+		show_subcontracting_fields(frm)
+		setup_item_queries(frm)
+		fetch_supplier_warehouse(frm)
 		override_create_buttons(frm)
 	},
+	is_subcontracted: frm => {
+		if (frm.doc.is_subcontracted) {
+			show_subcontracting_fields(frm)
+		}
+	},
+	company: frm => {
+		setup_item_queries(frm)
+		fetch_supplier_warehouse(frm)
+	},
+	supplier: frm => {
+		fetch_supplier_warehouse(frm)
+	},
 })
+
+// TODO: override when a qty changes in item table, it changes the fg_item_qty (assumes same UOM)
+// TODO: subcontracting table: autofill fields when a row is manually added and user selects the WO
+// TODO: when subcontracting table row removed, adjust Item row fg_item_qty
+
+function show_subcontracting_fields(frm) {
+	if (!frm.doc.company || !frm.doc.is_subcontracted) {
+		hide_field('subcontracting')
+		return
+	}
+	if (
+		frappe.boot.inventory_tools &&
+		frappe.boot.inventory_tools[frm.doc.company] &&
+		frappe.boot.inventory_tools[frm.doc.company].enable_work_order_subcontracting
+	) {
+		unhide_field('subcontracting')
+		setTimeout(() => {
+			frm.remove_custom_button('Purchase Receipt', 'Create')
+			frm.remove_custom_button('Subcontracting Order', 'Create')
+		}, 1000)
+	} else {
+		hide_field('subcontracting')
+	}
+}
+
+function setup_item_queries(frm) {
+	frm.set_query('item_code', 'items', () => {
+		if (me.frm.doc.is_subcontracted) {
+			var filters = { supplier: me.frm.doc.supplier }
+			if (me.frm.doc.is_old_subcontracting_flow) {
+				filters['is_sub_contracted_item'] = 1
+			} else {
+				if (
+					frappe.boot.inventory_tools &&
+					frappe.boot.inventory_tools[frm.doc.company] &&
+					frappe.boot.inventory_tools[frm.doc.company].enable_work_order_subcontracting
+				) {
+					filters['is_stock_item'] = 0
+				}
+			}
+			return {
+				query: 'erpnext.controllers.queries.item_query',
+				filters: filters,
+			}
+		} else {
+			return {
+				query: 'erpnext.controllers.queries.item_query',
+				filters: { supplier: me.frm.doc.supplier, is_purchase_item: 1, has_variants: 0 },
+			}
+		}
+	})
+}
+
+function setup_supplier_warehouse_query(frm) {
+	frm.set_query('supplier_warehouse', () => {
+		return {
+			filters: { is_group: 0 },
+		}
+	})
+}
+
+function fetch_supplier_warehouse(frm) {
+	if (!frm.doc.company || !frm.doc.supplier) {
+		return
+	}
+	frappe
+		.xcall('inventory_tools.inventory_tools.overrides.purchase_invoice.fetch_supplier_warehouse', {
+			company: frm.doc.company,
+			supplier: frm.doc.supplier,
+		})
+		.then(r => {
+			if (r && r.message) {
+				frm.set_value('supplier_warehouse', r.message.supplier_warehouse)
+			}
+		})
+}
 
 function override_create_buttons(frm) {
 	if (!frm.doc.multi_company_purchase_order || frm.doc.docstatus != 1) {
 		return
 	}
-
 	let aggregated_purchasing_warehouse = undefined
-	frappe.db.get_value('Buying Settings', 'Buying Settings', 'aggregated_purchasing_warehouse').then(r => {
-		aggregated_purchasing_warehouse = r.message.aggregated_purchasing_warehouse
-		if (!aggregated_purchasing_warehouse) {
-			frm.remove_custom_button('Purchase Invoice', 'Create')
-			frm.remove_custom_button('Purchase Receipt', 'Create')
-			frm.remove_custom_button('Payment', 'Create')
-			frm.remove_custom_button('Payment Request', 'Create')
-			frm.remove_custom_button('Subscription', 'Create')
-			frm.add_custom_button(
-				'Create Purchase Invoices',
-				async () => {
-					await create_pis(frm)
-				},
-				'Create'
-			)
-			frm.add_custom_button(
-				'Create Purchase Receipts',
-				async () => {
-					await create_prs(frm)
-				},
-				'Create'
-			)
-		} else {
-			frm.add_custom_button(
-				'Intercompany Sale and Transfer',
-				async () => {
-					await create_sis(frm)
-				},
-				'Create'
-			)
-		}
-	})
+	if (
+		frm.doc.docstatus &&
+		frappe.boot.inventory_tools &&
+		frappe.boot.inventory_tools[frm.doc.company] &&
+		frappe.boot.inventory_tools[frm.doc.company].aggregated_purchasing_warehouse
+	) {
+		aggregated_purchasing_warehouse = frappe.boot.inventory_tools[frm.doc.company].aggregated_purchasing_warehouse
+	}
+	if (!aggregated_purchasing_warehouse) {
+		frm.remove_custom_button('Purchase Invoice', 'Create')
+		frm.remove_custom_button('Purchase Receipt', 'Create')
+		frm.remove_custom_button('Payment', 'Create')
+		frm.remove_custom_button('Payment Request', 'Create')
+		frm.remove_custom_button('Subscription', 'Create')
+		frm.add_custom_button(
+			'Create Purchase Invoices',
+			async () => {
+				await create_pis(frm)
+			},
+			'Create'
+		)
+		frm.add_custom_button(
+			'Create Purchase Receipts',
+			async () => {
+				await create_prs(frm)
+			},
+			'Create'
+		)
+	} else {
+		frm.add_custom_button(
+			'Intercompany Sale and Transfer',
+			async () => {
+				await create_sis(frm)
+			},
+			'Create'
+		)
+	}
 }
 
 async function create_pis(frm) {
