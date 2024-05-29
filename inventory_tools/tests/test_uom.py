ef0410addf1b2f35cfda3a3d416bfa3087b05de6
Tyler Matteson <support@agritheory.dev>
2024-05-29 13:42:53 -0400

    Test alternative workstation (#80)
    * test: add test for alternative workstation
    
    * feat: make alternative workstations configurable
    
    * fix: uncomment js code for testing
    
    * feat: search alternative workstation names
    
    * refactor: pop filters that cause error for Workstation
    
    * chore: update override commit hash
    
    ---------
    
    Co-authored-by: Heather Kusmierz <heather.kusmierz@gmail.com>

--- inventory_tools/tests/test_uom.py
+++ inventory_tools/tests/test_uom.py
@@ -17,3 +17,27 @@ def test_uom_enforcement_validation():
 		so.save()
 
 	assert "Invalid UOM" in exc_info.value.args[0]
+
+
+@pytest.mark.order(41)
+def test_uom_enforcement_query():
+	inventory_tools_settings = frappe.get_doc(
+		"Inventory Tools Settings", frappe.defaults.get_defaults().get("company")
+	)
+	inventory_tools_settings.enforce_uoms = True
+	inventory_tools_settings.save()
+	frappe.call(
+		"frappe.desk.search.search_link",
+		**{
+			"doctype": "UOM",
+			"txt": "",
+			"query": "inventory_tools.inventory_tools.overrides.uom.uom_restricted_query",
+			"filters": {"parent": "Parchment Paper"},
+			"reference_doctype": "Purchase Order Item",
+		},
+	)
+	assert len(frappe.response.results) == 2
+	assert frappe.response.results[0].get("value") == "Nos"
+	assert frappe.response.results[0].get("description") == "1.0"
+	assert frappe.response.results[1].get("value") == "Box"
+	assert frappe.response.results[1].get("description") == "100.0"
